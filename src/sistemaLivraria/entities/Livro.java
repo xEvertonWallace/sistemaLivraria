4175e7ed89ceac9f057e94b7b5f158349d7d7c14
Everton Oliveira <evertonwallace@consite.com.br>
2023-10-27 22:41:24 -0300

    Classe LivroDidatico alterado para Titulo e classe Livro recebeu modificacoes

--- src/sistemaLivraria/entities/Livro.java
+++ src/sistemaLivraria/entities/Livro.java
@@ -1,50 +1,21 @@
 package sistemaLivraria.entities;
 
-public class Livro {
-	private String nome;
-	private String autorPrincipal;
-	private String editora;
-	private String resumo;
-	private double preco;
+class Livro extends Titulo {
+    private String autor;
+    private String resumo;
+    private String area;
 
-	public Livro(String nome, String autorPrincipal, String editora, String resumo, double preco) {
-		this.nome = nome;
-		this.autorPrincipal = autorPrincipal;
-		this.editora = editora;
-		this.resumo = resumo;
-		this.preco = preco;
+    public Livro(String nome, String autor, String editora, String resumo, double preco, String area) {
+        super(nome, editora, preco);
+        this.autor = autor;
+        this.resumo = resumo;
+        this.area = area;
+    }
 
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public double getPreco() {
-		return preco;
-	}
-
-	
-
-	public double calcularPrecoFinal() {
-		double iss = 0;
-		double precoFinal = 0;
-
-		if (this instanceof LivroDidatico) {
-			iss = this.preco * 0.1;
-			precoFinal = this.preco + iss;
-		} else {
-			iss = this.preco * 0.03;
-			precoFinal = this.preco + iss;
-		}
-
-		return precoFinal;
-	}
-
-	@Override
-	public String toString() {
-		return "Livro: " + nome + "\nAutor Principal: " + autorPrincipal + "\nEditora: " + editora + "\nResumo: "
-				+ resumo + "\nPreço Original: " + preco + "\nValor do ISS: " + calcularPrecoFinal() + "\nPreço Final: "
-				+ calcularPrecoFinal();
-	}
+    public void exibirInformacoes() {
+        super.exibirInformacoes();
+        System.out.println("Autor: " + autor);
+        System.out.println("Resumo: " + resumo);
+        System.out.println("Área: " + area);
+    }
 }
---
+++ src/sistemaLivraria/entities/Titulo.java
@@ -0,0 +1,19 @@
+package sistemaLivraria.entities;
+
+class Titulo {
+    protected String nome;
+    protected String editora;
+    protected double preco;
+
+    public Titulo(String nome, String editora, double preco) {
+        this.nome = nome;
+        this.editora = editora;
+        this.preco = preco;
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("Título: " + nome);
+        System.out.println("Editora: " + editora);
+        System.out.println("Preço: " + preco);
+    }
+}
